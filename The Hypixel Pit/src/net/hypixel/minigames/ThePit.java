425128fa8871aacf32fcffcad7d0932737a056c5
BuildTools <unconfigured@null.spigotmc.org>
2018-11-18 20:36:05 +0900

    20:35 / The Pit

--- The Hypixel Pit/src/net/hypixel/minigames/ThePit.java
+++ The Hypixel Pit/src/net/hypixel/minigames/ThePit.java
@@ -67,8 +67,6 @@ public class ThePit extends JavaPlugin {
 		
 		PerkManager.registerPerks();
 		
-		
-		
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new BukkitRunnable() {
 			
 			@Override
