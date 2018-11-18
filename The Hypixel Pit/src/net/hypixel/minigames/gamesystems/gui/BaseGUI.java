d27ae7d0493175403631d426e7fe0667f863c792
king <king@1.229.17.23>
2018-11-18 09:40:47 +0900

    Add GUI Base

---
+++ The Hypixel Pit/src/net/hypixel/minigames/gamesystems/gui/BaseGUI.java
@@ -0,0 +1,166 @@
+package net.hypixel.minigames.gamesystems.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
+
+import net.hypixel.minigames.ThePit;
+
+public class BaseGUI {
+	private String name;
+	private int size;
+	private GUIClickEventHandler handler;
+	private String[] optionNames;
+	private ItemStack[] optionIcons;
+
+	public BaseGUI(String name, int size, GUIClickEventHandler handler) {
+        this.name = name;
+        this.size = size;
+        this.handler = handler;
+        this.optionNames = new String[size];
+        this.optionIcons = new ItemStack[size];
+    }
+
+	public BaseGUI setOption(int position, ItemStack icon, String name, String[] info) {
+		this.optionNames[position] = name;
+		this.optionIcons[position] = ItemUtils.name(icon, name, info);
+		return this;
+	}
+
+	public void open(Player player) {
+		Inventory inventory = Bukkit.createInventory(player, this.size, this.name);
+		for (int iii = 0; iii < this.optionIcons.length; iii++) {
+			if (this.optionIcons[iii] != null) {
+				inventory.setItem(iii, this.optionIcons[iii]);
+			}
+		}
+		player.openInventory(inventory);
+	}
+
+	public void update(Player player) {
+		InventoryView inventory = player.getOpenInventory();
+		if (inventory != null) {
+			for (int iii = 0; iii < this.optionIcons.length; iii++) {
+				if (this.optionIcons[iii] != null) {
+					inventory.setItem(iii, this.optionIcons[iii]);
+				}
+			}
+			player.updateInventory();
+		}
+	}
+
+	public void destroy() {
+		this.handler = null;
+		this.optionNames = null;
+		this.optionIcons = null;
+	}
+
+	public void onInventoryClick(InventoryClickEvent event) {
+		if (!event.getInventory().getTitle().equals(name)) {
+			return;
+		}
+
+		event.setCancelled(true);
+
+		int slot = event.getRawSlot();
+
+		try {
+			if (!(slot >= 0 && slot < size && optionNames[slot] != null)) {
+				return;
+			}
+		} catch (NullPointerException e) {
+			return;
+		}
+
+		GUIClickEvent clickEvent = new GUIClickEvent((Player) event.getWhoClicked(), slot, optionNames[slot]);
+		handler.onOptionClick(clickEvent);
+
+		if (clickEvent.willClose()) {
+			final Player player = (Player) event.getWhoClicked();
+
+			Bukkit.getScheduler().runTaskLater(ThePit.getInstance(), new Runnable() {
+				@Override
+				public void run() {
+					player.closeInventory();
+				}
+			}, 1L);
+		}
+
+		if (clickEvent.willDestroy()) {
+			destroy();
+		}
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String[] getOptions() {
+		return optionNames;
+	}
+
+	public static class GUIClickEvent {
+		private Player player;
+		private int position;
+		private String name;
+		private boolean close;
+		private boolean destroy;
+
+		public GUIClickEvent(Player player, int position, String name) {
+			this.player = player;
+			this.position = position;
+			this.name = name;
+			this.close = false;
+			this.destroy = false;
+		}
+
+		public Player getPlayer() {
+			return player;
+		}
+
+		public void setPlayer(Player player) {
+			this.player = player;
+		}
+
+		public int getPosition() {
+			return position;
+		}
+
+		public void setPosition(int position) {
+			this.position = position;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public boolean willClose() {
+			return close;
+		}
+
+		public void setClose(boolean close) {
+			this.close = close;
+		}
+
+		public boolean willDestroy() {
+			return destroy;
+		}
+
+		public void setDestroy(boolean destroy) {
+			this.destroy = destroy;
+		}
+
+	}
+
+	public static abstract interface GUIClickEventHandler {
+		public abstract void onOptionClick(GUIClickEvent event);
+	}
+
+}
---
+++ The Hypixel Pit/src/net/hypixel/minigames/gamesystems/gui/GUIListener.java
@@ -0,0 +1,71 @@
+package net.hypixel.minigames.gamesystems.gui;
+
+import java.util.HashSet;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import com.google.common.collect.Maps;
+
+import net.hypixel.minigames.gamesystems.gui.BaseGUI.GUIClickEventHandler;
+
+public class GUIListener implements Listener {
+
+	private final Map<Player, BaseGUI> guis = Maps.newHashMap();
+
+	public void create(Player player, String name, int size, GUIClickEventHandler handler) {
+		if (player != null) {
+			destroy(player);
+			guis.put(player, new BaseGUI(name, size, handler));
+		}
+	}
+
+	public BaseGUI getMenu(Player player) {
+		return guis.get(player);
+	}
+
+	public void show(Player player) {
+		if (guis.containsKey(player)) {
+			guis.get(player).open(player);
+		}
+	}
+
+	public void update(final Player player) {
+		if (guis.containsKey(player)) {
+			guis.get(player).update(player);
+		}
+	}
+
+	public void setOption(Player player, int position, ItemStack icon, String name, String... info) {
+		if (guis.containsKey(player)) {
+			guis.get(player).setOption(position, icon, name, info);
+		}
+	}
+
+	public String[] getOptions(Player player) {
+		if (guis.containsKey(player)) {
+			return guis.get(player).getOptions();
+		}
+		return null;
+	}
+
+	public void destroy(Player player) {
+		if (guis.containsKey(player)) {
+			guis.remove(player).destroy();
+			player.getOpenInventory().close();
+		}
+	}
+
+	public void destroyAll() {
+		for (Player player : new HashSet<Player>(guis.keySet())) {
+			destroy(player);
+		}
+	}
+
+	public boolean has(Player player) {
+		return guis.containsKey(player);
+	}
+
+}
